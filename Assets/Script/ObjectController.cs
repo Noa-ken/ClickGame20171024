de959f129f5f5153dd1697410279651e295b34b8
Kenshin Hayashi <nkc.kenshin.hayashi@gmail.com>
2017-10-26 11:15:02 +0900

    スクリプト追加

---
+++ Assets/Script/ObjectController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Script/ObjectGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectGenerator : MonoBehaviour
+{
+    public GameObject Objectball;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Objectball = Instantiate(gameObject) as GameObject;
+
+        }
+/*
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 worldDir = ray.direction;
+        Objectball.GetComponent<ObjectController>().Shoot(worldDir.normalized * 2000);
+
+*/
+    }
+
+}
